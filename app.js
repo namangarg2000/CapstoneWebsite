da45de705b00dc26e8b9e87daf7a39579b9110ab
Naman Garg <namangarg155@gmail.com>
2022-08-14 03:09:26 +0530

    Almost finished admin panel

--- app.js
+++ app.js
@@ -3,12 +3,19 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app  = express();
 app.use(express.static("static"))
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
+
+const apiURL = "https://serene-ridge-19523.herokuapp.com/";
+let totalinlibrary = 0;
+let insidemorethanone = 0;
+let firsttime = 0;
+let totalregistered = 0;
 
 app.get("/", (req,res)=>{
   res.render("index");
@@ -26,8 +33,8 @@ app.post("/login", (req, res) => {
   console.log(email, password);
 
   if(email == 'admin@admin' && password == "admin"){
-    //res.sendFile(__dirname + "/success.html");
-    res.render("success")
+    res.redirect("/admin");
+    //res.render("admin",{laststatus: "Entry"});
   }
     else{
       console.log("LOGIN FAILED");
@@ -44,6 +51,48 @@ app.get("/developmentTeam", (req,res) =>{
   //res.sendFile(__dirname + "/developmentTeam.html");
   res.render("about");
 })
+
+app.get("/admin",function(req,res){
+
+
+    https.get(apiURL,function(response){  
+      console.log(response.statusCode);
+
+      
+
+      response.on("data", function(data){
+        data = JSON.parse(data);  //Here we receive the data from the API - of all the students
+        data.forEach(person => {
+          totalregistered+=1;
+
+          if(person.laststatus === "Entry"){
+            totalinlibrary+=1;
+          }
+
+          let timeinsidehour = 0;
+          if(person.laststatus === "Entry" && timeinsidehour > 1){
+            insidemorethanone+=1;
+          }
+
+          if(person.entry != "" && person.entry.split(",").length === 1){
+            firsttime+=1;
+          }
+
+        });
+        res.render("admin",{laststatus: "", totalinlibrary: totalinlibrary, insidemorethanone: insidemorethanone, firsttime: firsttime, totalregistered: totalregistered});
+      }); 
+    });
+});
+
+app.post("/admin", (req,res) => {
+  const requestedRoll = req.body.roll;
+  https.get(apiURL + "roll/" + requestedRoll, function(response){
+    response.on("data", function(data){
+      data = JSON.parse(data);
+      res.render("admin",{laststatus: data.laststatus, totalinlibrary: totalinlibrary, insidemorethanone: insidemorethanone, firsttime: firsttime, totalregistered: totalregistered});
+    });
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server Running on localhost:3000")
