e68668a2ea5a33c40097478b4ae93ed8c3bf91b0
Naman Garg <namangarg155@gmail.com>
2022-12-18 19:02:00 +0530

    user

---
+++ test.js
@@ -0,0 +1,8 @@
+let d = Date.now();
+let str = ""+d;
+let ONE_HOUR = 3600000;
+console.log(d - 1671366600000 > ONE_HOUR)
+let x = parseFloat(str)
+console.log(x);
+console.log(typeof(str));
+console.log(typeof(x));
